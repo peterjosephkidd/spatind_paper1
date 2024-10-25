757a4d92dc2573becfc037c6bee4bdcc52e86b65
Peter Kidd <peter.kidd@cefas.gov.uk>
2024-10-25 13:06:21 +0100

    Changes mostly to paths. This runs fine.

--- Scripts/data_3_CleanExchange.R
+++ Scripts/data_3_CleanExchange.R
@@ -3,6 +3,13 @@
 #>                      3. Process DATRAS Survey data
 #>                           
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#> This script loads DATRAS survey data and performs some basic data cleaning 
+#> and manipulation. This script also merges the hh and hl exchange data into 
+#> hlhh which is used throughout further scripts. ICES divisions are also added
+#> to the survey data so that we know what rectangle belongs in which division. 
+#> This is important when we come to filter survey data to a stocks management 
+#> area (e.g. 27.7d for ple.27.7d). We calculate indicators later on based on 
+#> data from within the management area only. 
 
 library(writexl)
 library(readxl)
@@ -14,39 +21,40 @@ library(stringr)
 rm(list = ls())
 
 # Set-up ####
-load.path <- "~/OneDrive - CEFAS/Projects/C8503B/PhD/SpatIndAssess(GIT)/SpatIndAssess/Data/DR_Stocks/"
-save.path <- "~/OneDrive - CEFAS/Projects/C8503B/PhD/SpatIndAssess(GIT)/SpatIndAssess/Data/DR_Stocks/SurveyData/"
+load.path <- paste0(getwd(), "/Data/Initial/")
+save.path <- paste0(getwd(), "/Data/Generated/")
 
-stksurveys <- read_xlsx(paste0(load.path, "icesSA_data/icesData-AllSurveyData-manual.xlsx"), sheet = "Surveys")
-refpts     <- read_xlsx(paste0(load.path, "icesSA_data/icesData-69stks-AY2022-stkdescrptn.xlsx"))
-sa_data    <- read_xlsx(paste0(load.path, "icesSA_data/icesData-69stks-AY2022-SA-data.xlsx"))
+stksurveys <- read_xlsx(paste0(load.path, "DR_Stocks/StockInfo/icesData-AllSurveyData-manual.xlsx"), sheet = "Surveys")
+refpts     <- read_xlsx(paste0(load.path, "DR_Stocks/StockInfo/icesData-AllSurveyData-manual.xlsx"), sheet = "Stocks")
 
-load("~/OneDrive - CEFAS/Projects/C8503B/PhD/spatind-1/boot/initial/data/ices_shp/ICES Divs/ices_divs.rds")
-load("~/OneDrive - CEFAS/Projects/C8503B/PhD/spatind-1/boot/initial/data/ices_shp/ICES Rect/ices_rect.rds")
+load(paste0(load.path, "/ICES Divs/ices_divs.rds"))
+load(paste0(load.path, "/ICES Rect/ices_rect.rds"))
+
 source("C:/Users/pk02/OneDrive - CEFAS/Projects/C8503B/PhD/SpatIndAssess(GIT)/SpatIndAssess/Functions/dataprep_funs.R")
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Check stock divisions exist in ICES rect shapefile
-areas <- sort(unique(ices_rect$Area_27))
-divs <- unique(unlist(strsplit(stksurveys$Divisions, ", ")))
+areas    <- sort(unique(ices_rect$Area_27))
+divs     <- unique(unlist(strsplit(stksurveys$Divisions, ", ")))
 missdivs <- divs[!divs %in% areas]
 missdivs
 rm(areas, divs, missdivs)
 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # load(paste0(save.path, "/stks.rds")) # saved stks from data_2_DownloadDATRAS
-# stks <- "ank.27.78abd"               # method 1, to individual stocks
-stks <- "all.stocks"                 # method 2
-# Clear dir
-do.call(file.remove, list(list.files(paste0(save.path, stks, "/clean/"), full.names = TRUE)))
+# stks <- "ank.27.78abd"               # method 1, to run for individual stocks
+stks <- "all.stocks"                   # method 2, USE THIS
 
-# Clean data ####
+# Start with clear directory (might be issues with permissions)
+#do.call(file.remove, list(list.files(paste0(save.path, "DR_Stocks/SurveyData/Cleaned/"), full.names = TRUE)))
+
+# Clean survey data ####
 for (i in 1:length(stks)) {
   stk <- stks[i]
   message(stk)
   
   if(stk == "all.stocks") {
-    srvy.list <- unique(str_split_i(list.files(paste0(load.path, "SurveyData/", stk, "/raw/")), pattern = ".Yr", i=1))
+    srvy.list <- unique(str_split_i(list.files(paste0(load.path, "DR_Stocks/SurveyData/", stk)), pattern = ".Yr", i=1))
   } else {
     srvys <- stksurveys %>%
       filter(StockKeyLabel == stk) %>%
@@ -54,18 +62,20 @@ for (i in 1:length(stks)) {
     srvy.list <- unique(srvys$SurveyAcronymn)
   }
   
-  suppressWarnings(dir.create(paste0(save.path, stk, "/clean"), recursive = T))
-
+  # Create directory to save cleaned survey data
+  suppressWarnings(dir.create(paste0(save.path, "DR_Stocks/SurveyData/Cleaned/", stk), recursive = T))
+  
   for (srvy in 1:length(srvy.list)) {
     
     srv <- srvy.list[srvy]
     message(srv)
+    
     # Load survey data 
-    files <- list.files(paste0(load.path, "SurveyData/", stk, "/raw/"), pattern = paste0("^", srv, "\\.Yr", "*"), full.names = TRUE)
+    files <- list.files(paste0(load.path, "DR_Stocks/SurveyData/", stk), pattern = paste0("^", srv, "\\.Yr", "*"), full.names = TRUE)
 
-    load(files[str_detect(files, ".HH--")])
-    load(files[str_detect(files, ".HL--")])
-    load(files[str_detect(files, ".CA--")])
+    load(files[str_detect(files, "\\.HH--")])
+    load(files[str_detect(files, "\\.HL--")])
+    load(files[str_detect(files, "\\.CA--")])
     
     # Remove duplicates
     hh <- unique(hh)
@@ -108,12 +118,12 @@ for (i in 1:length(stks)) {
       mutate(SumHLNoAtLngt = sum(HLNoAtLngt),
              "SumHlNoAtLngt - TotalNo" = SumHLNoAtLngt - TotalNo) # should = 0    
     
-    any(hlhh$`SumHlNoAtLngt - TotalNo` != 0)
+    #any(hlhh$`SumHlNoAtLngt - TotalNo` != 0)
     
-    save(hh,   file = paste0(save.path, stk, "/clean/", namefile(stk, hh)))
-    save(hl,   file = paste0(save.path, stk, "/clean/", namefile(stk, hl)))
-    save(ca,   file = paste0(save.path, stk, "/clean/", namefile(stk, ca)))
-    save(hlhh, file = paste0(save.path, stk, "/clean/", namefile(stk, hlhh, r = "HLHH")))
+    save(hh,   file = paste0(save.path, "DR_Stocks/SurveyData/Cleaned/", stk, "/", namefile(stk, hh)))
+    save(hl,   file = paste0(save.path, "DR_Stocks/SurveyData/Cleaned/", stk, "/", namefile(stk, hl)))
+    save(ca,   file = paste0(save.path, "DR_Stocks/SurveyData/Cleaned/", stk, "/", namefile(stk, ca)))
+    save(hlhh, file = paste0(save.path, "DR_Stocks/SurveyData/Cleaned/", stk, "/", namefile(stk, hlhh, r = "HLHH")))
   
   }
 }
