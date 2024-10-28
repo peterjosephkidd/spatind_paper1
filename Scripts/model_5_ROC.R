1f0283d98e3ba9177d6d13d266f26d0de599a17a
Peter Kidd <peter.kidd@cefas.gov.uk>
2024-10-28 18:06:53 +0000

    Added in section from plot_7 to select the surveys wihth contrast and longevity. Genereal clean.

--- Scripts/model_5_ROC.R
+++ Scripts/model_5_ROC.R
@@ -3,6 +3,9 @@
 #>               5. Assess Spatial Indicators
 #> 
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#> This script takes the indicator calculations from model_4_SpatIndCalc.R
+#> and compares them to SSB stock assessment outputs using Receiver Operating
+#> Characteristic (ROC curves).
 
 library(dplyr)
 library(icesVocab)
@@ -11,43 +14,37 @@ library(ggplot2)
 
 rm(list = ls())
 
-load.path <- "C:/Users/pk02/OneDrive - CEFAS/Projects/C8503B/PhD/SpatIndAssess(GIT)/SpatIndAssess/"
-save.path <- "C:/Users/pk02/OneDrive - CEFAS/Projects/C8503B/PhD/SpatIndAssess(GIT)/SpatIndAssess/Data/DR_Stocks/Outputs/"
+# Load Data
+sa_data  <- read_xlsx(paste0(getwd(), "/Data/Initial/DR_Stocks/StockInfo/icesData-69stks-AY2022-SA-data.xlsx")) # Stock Assessment Data 
+load(paste0(getwd(), "/Output/Data/SpatInds/spatinds_4.rds")) # Spatial Indicator Data
+load(paste0(getwd(), "/Data/Generated/DR_Stocks/StockNames/stk_names_3a.rds")) # stk_names from data_3a_Mature.R
+source(paste0(getwd(), "/Functions/ROC_funs.R")) # ROC Functions
 
-# Stock Assessment Data 
-sa_data <- read_xlsx(paste0(load.path, "Data/DR_Stocks/icesSA_data/icesData-69stks-AY2022-SA-data.xlsx"))
+# Check we are not missing any stocks
+identical(stk_names, unique(spatinds$StockKeyLabel))
+all(stk_names %in% sa_data$StockKeyLabel)
 
-# Spatial Indicator Data
-si.files <- list.files(paste0(load.path, "Data/DR_Stocks/Outputs/Spatinds/"), full.names = T)
-load(si.files[1])
-
-# ROC Functions
-source(paste0(load.path, "Functions/ROC_funs.R")) 
-
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
+# Combine SSB, MSY Btrigger, and Spatinds data
 ssb.ref <- sa_data %>% 
   select(StockKeyLabel, Year, SSB, MSYBtrigger) %>%
-  filter(StockKeyLabel %in% unique(spatinds$StockKeyLabel)) %>%
+  filter(StockKeyLabel %in% stk_names) %>%
   mutate(`SSB/MSYBtrigger` = SSB/MSYBtrigger) %>%
   full_join(., spatinds, by = c("StockKeyLabel", "Year")) %>%
-  mutate(SurveyNameIndex = paste0(SurveyName, ", ", SurveyIndex))
-
-rbind(head(ssb.ref), tail(ssb.ref))
+  mutate(SurveyNameIndex = paste0(SurveyName, ", ", SurveyIndex)) %>%
+  filter(SurveyNameIndex != "NA, NA")
 
 all(unique(spatinds$StockKeyLabel) %in% unique(ssb.ref$StockKeyLabel))
 
-# Parameters
+# Parameters for ROC
 state <- "SSB/MSYBtrigger"
 inds <- c("CoG (x)", "CoG (y)", "Inertia", "EOO", "ELA", "POPR", "POPH", "Gini Index", "D95", "SA", "EA", "SPI")
-stks <- unique(ssb.ref$StockKeyLabel)
 roc_longlist <- list()
 iter <- 1
 
 # Loop
-for (i in 1:length(stks)) {
+for (i in 1:length(stk_names)) {
   
-  stk <- stks[i]
+  stk <- stk_names[i]
   #stk <- "ple.27.7a"
   #i <- which(stks == stk)
   ssb.ref.stk <- filter(ssb.ref, StockKeyLabel == stk)
@@ -58,9 +55,11 @@ for (i in 1:length(stks)) {
     srvindx <- srvys[j]
     ssb.ref.stk.srvindx <- filter(ssb.ref.stk, SurveyNameIndex == srvindx)
     qrs <- sort(unique(ssb.ref.stk.srvindx$Quarter))
+    
     if (length(qrs) == 0) {qrs = NA}
     
     for (w in 1:length(qrs)) {
+      
       qr <- qrs[w]
       message(paste0("#>>>>>> ", stk, ": ", srvindx, ", Quarter: ", qr, " >>>>>>#"))
       
@@ -71,6 +70,7 @@ for (i in 1:length(stks)) {
       }
       
       L50levels <- unique(ssb.ref.stk.srvindx.qr$L50lvl)
+      
       if (length(L50levels) == 0) {L50levels = NA}
       
       for (lvl in 1:length(L50levels)) {
@@ -78,6 +78,7 @@ for (i in 1:length(stks)) {
         L50 <- L50levels[lvl]
         ssb.ref.stk.srvindx.qr.lvl <- filter(ssb.ref.stk.srvindx.qr, L50lvl == L50) 
         
+        # ROC Analysis
         roc_long <- rocR(ssb.ref.stk.srvindx.qr.lvl, state, inds, format.df = "long", p = F)
         roc_long$L50lvl <- L50
         roc_longlist[[iter]] <- roc_long
@@ -90,140 +91,108 @@ for (i in 1:length(stks)) {
 # Output
 rocAll_long <- do.call(rbind, roc_longlist)
 
+rocAll_long <- rocAll_long %>% 
+  mutate(across(where(is.list), as.numeric),
+         across(c(`SSB/MSYBtrigger`, `Spatial Indicator Value`), as.numeric))
+
+str(rocAll_long)
+
+# Check for NAs in AUC
+any(is.na(rocAll_long$AUC))
+rocAll_long[is.na(rocAll_long$AUC),]
+
+#> We see NaN in FPR. FPR = FP/(FP+TN). If stock status is only positive, then you 
+#> cannot compute FPR and thus TSS and AUC. We may see NaN in TPR if stock status is only
+#> negative and TPR, TSS, AUC, cannot be computed.
+#> 
+#> In other rows we see that FP, TP, TN, and FN are filled with NA. This is because
+#> the indicator is not calculated for these year
+#> 
+#> We will get rid of rows where the FPR or TPR = NaN, and rows where the 
+#> indicator value = NA
+
+rocAll_long <- rocAll_long %>% 
+  filter(!is.nan(TPR),
+         !is.nan(FPR),
+         !is.na(TP),
+         !is.na(FP),
+         !is.na(TN),
+         !is.na(TN))
+
+any(is.na(rocAll_long$AUC))
+
 # Save
-suppressWarnings(dir.create(paste0(save.path, "/ROC"), recursive = T))
-save(rocAll_long, file = paste0(save.path, "ROC/ROCdata.rda"))
+suppressWarnings(dir.create(paste0(getwd(), "/Output/Data/ROC"), recursive = T))
+save(rocAll_long, file = paste0(getwd(), "/Output/Data/ROC/ROCdata.rds"))
 
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# Apparent issue
-# There is spatial indicator data but no AUC for many cases
-missdata <- rocAll_long %>%
-  ungroup() %>%
-  filter(!is.na(`Spatial Indicator Value`),
-         is.na(AUC)) %>%
-  select(StockKeyLabel, Quarter, SurveyIndex, SurveyName) %>%
-  distinct() %>%
-  arrange(StockKeyLabel) 
+#> Not all surveys are informative. A long timeseries with contrast in stock
+#> status is ideal for telling us whether an indicator can reliably classify 
+#> stock status. We filter to surveys with at least 15 years of survey data
+#> and that there is contrast of a ratio of max 1:4
+## Filter to good surveys
 
-# But these are stocks with no stock status contrast in the time series that the spatial indicators are calculated for
-# Must be contrast in stock status for ROC to be run
-no.contrast <- rocAll_long %>% 
-  group_by(Quarter, StockKeyLabel, SurveyIndex, SurveyName, SurveyNameIndex, `Spatial Indicator`) %>%
-  filter(!is.na(`Spatial Indicator Value`), 
-         Year > 1000) %>%
-  mutate(contrast = if_else(all(status == TRUE), 0, 
-                            if_else(all(status == FALSE), 0, 1))) %>%
-  filter(contrast == 0)  %>%
-  ungroup() %>%
-  select(StockKeyLabel, Quarter, SurveyIndex, SurveyName) %>%
-  distinct() %>%
-  filter(!is.na(SurveyName)) %>%
-  arrange(StockKeyLabel) %>%
-  print(n = nrow(.))
-
-# The stocks in issues that are not accounted for in no contrast
-probstk <- missdata[!missdata$StockKeyLabel %in% no.contrast$StockKeyLabel,] # none
-
-View(rocAll_long %>%
-  filter(StockKeyLabel == probstk$StockKeyLabel[1],
-         Quarter == probstk$Quarter,
-         SurveyIndex == probstk$SurveyIndex,
-         SurveyName == probstk$SurveyName) %>%
-  filter(!is.na(`Spatial Indicator Value`),
-         is.na(AUC)))
-
-
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# Explore
-roc_summary <- rocAll_long %>%
-  select(StockKeyLabel, SurveyNameIndex, Quarter, `Spatial Indicator`, AUC, L50lvl) %>%
-  distinct() %>%
-  group_by(StockKeyLabel, SurveyNameIndex, Quarter, `Spatial Indicator`, L50lvl) %>%
+# How much contrast in SSB/MSYB trigger in each time series:
+ROContrast <- rocAll_long %>%
+  select(StockKeyLabel, Year, Quarter, SurveyNameIndex, status, Value) %>%
   na.omit() %>%
-  summarise(AUC) %>%
-  print(n = nrow(.))
-
-table(roc_summary$`Spatial Indicator`)
-
-roc_summary %>%
-  group_by(`Spatial Indicator`, L50lvl) %>%
-  summarise(n.good = length(which(AUC >= 0.75)),
-            n.avg  = length(which(AUC < 0.75 & AUC >= 0.35)),
-            n.bad  = length(which(AUC < 0.35))) %>%
-  arrange(L50lvl, -n.good) %>%
-  print(n = nrow(.))
-
-roc_summary %>%
-  group_by(StockKeyLabel, L50lvl) %>%
-  summarise(n.good = length(which(AUC >= 0.75)),
-            n.avg  = length(which(AUC < 0.75 & AUC >= 0.35)),
-            n.bad  = length(which(AUC < 0.35))) %>%
-  arrange(L50lvl, -n.good) %>%
-  print(n = nrow(.))
-
-# Years of data requested vs actual
-stksurveys <- read_xlsx(paste0(load.path, "Data/DR_Stocks/icesSA_data/icesData-AllSurveyData-manual.xlsx"), sheet = "Surveys")
-
-stksurveys %>%
-  select(StockKeyLabel, SurveyAcronymn, SurveyIndex, YearStart, YearEnd, Quarter, InDatras, Divisions) %>%
-  filter(InDatras == 1) %>%
+  select(-Value) %>%
   distinct() %>%
-  na.omit() %>%
-  rename(SurveyName  = SurveyAcronymn,
-         YearStrtReq = YearStart,
-         YearEndReq  = YearEnd)
+  group_by(StockKeyLabel, SurveyNameIndex, Quarter) %>%
+  summarise(N.years = length(Year),
+            N.poor  = length(which(status == FALSE)),
+            N.good  = length(which(status == TRUE)),
+            Ratio.P2G   = N.poor/N.good) %>%
+  print(n = nrow(.))
 
-YrsAct <- rocAll_long %>%
-  filter(!is.na(SurveyName),
-         Year > 1000) %>%
-  select(-c(`SSB/MSYBtrigger`, status, TP, FP, TN, FN, TPR, FPR, TSS, `Spatial Indicator Value`)) %>%
-  group_by(Quarter, StockKeyLabel, SurveyIndex, SurveyName, SurveyNameIndex, AUC, `Spatial Indicator`) %>%
-  mutate(YearStrtAct = min(Year),
-         YearEndAct  = max(Year))
+hist(ROContrast$Ratio.P2G[ROContrast$Ratio.P2G < 15], breaks = 100)
 
-YrsAct <- YrsAct %>%
-  group_by(Quarter, StockKeyLabel, SurveyIndex, SurveyName, SurveyNameIndex, AUC, `Spatial Indicator`) %>%
-  mutate(YearSeries = paste0(sort(Year), collapse = ", "),
-         YearRange  = paste0(YearStrtAct:YearEndAct, collapse = ", ")) %>%
-  select(-Year) %>%
+# Which stocks/surveys should be removed?
+## Remove if:
+### Less than 15 years in time series
+### More than 4:1 ratio of good:bad (or vice versa)
+removals <- ROContrast %>%
+  mutate(contrast = if_else(Ratio.P2G == 0 | is.infinite(Ratio.P2G ), 9, if_else(Ratio.P2G < 0.25 | Ratio.P2G > 4, 4 , 0)),
+         years    = if_else(N.years < 15, 1, 0),
+         rem      = contrast + years) %>%
+  arrange(-rem, N.years, Ratio.P2G) %>%
+  print(n = nrow(.))
+
+# Remove these stocks from data
+rocRem_long <- merge(rocAll_long, removals) %>%
+  filter(!rem > 0)
+
+save(rocRem_long, file = paste0(getwd(), "/Output/Data/ROC/rocRem_long.rds"))
+
+# Number of stocks remaining (25 stocks)
+stk_names_rem <- unique(rocRem_long$StockKeyLabel)
+stk_names_rem
+save(stk_names_rem, file = paste0(getwd(), "/Output/Data/ROC/rocRem_long_stocks.rds"))
+
+# Surveys (52)
+srvys_rem <- rocRem_long %>% 
+  select(StockKeyLabel, SurveyName, Quarter, SurveyNameIndex) %>% 
   distinct() %>%
-  ungroup() %>%
-  mutate(t = if_else(
-    is.null(dim(unlist(strsplit(YearRange, ", "))[unlist(strsplit(YearRange, ", ")) %in% unlist(strsplit(YearSeries, ", "))])),
-    "none", 
-    paste0(unlist(strsplit(YearRange, ", "))[unlist(strsplit(YearRange, ", ")) %in% unlist(strsplit(YearSeries, ", "))], collapse = ", ")
-  ))
+  arrange(StockKeyLabel)
+srvys_rem
+save(srvys_rem, file = paste0(getwd(), "/Output/Data/ROC/rocRem_long_surveys.rds"))
 
-unlist(strsplit(YrsAct$YearRange[1], ", "))[!unlist(strsplit(YrsAct$YearRange[1], ", ")) %in% unlist(strsplit(YrsAct$YearSeries[1], ", "))]
-is.null(dim(unlist(strsplit(YrsAct$YearRange[1], ", "))[!unlist(strsplit(YrsAct$YearRange[1], ", ")) %in% unlist(strsplit(YrsAct$YearSeries[1], ", "))]))
+# Quick plot
+#load(paste0(getwd(), "/Output/Data/ROC/ROCdata.rds"))
 
-         YearMiss   = if_else(is.null(dim(as.integer(unlist(strsplit(YearRange, ",")))[
-           !as.integer(unlist(strsplit(YearRange, ", "))) %in% 
-             as.integer(unlist(strsplit(YearSeries, ", ")))])), "none", paste0(as.integer(unlist(strsplit(YearRange, ", ")))[
-               !as.integer(unlist(strsplit(YearRange, ", "))) %in% 
-                 as.integer(unlist(strsplit(YearSeries, ", ")))])
+stk <- sample(stk_names, 1) # or specify
+plot_df <- filter(rocRem_long, 
+                  StockKeyLabel == stk, 
+                  L50lvl == "mean", 
+                  Year > 1500) %>%
+  select(-c(Year, `SSB/MSYBtrigger`, status, TP, FP, TN, FN, FPR, TPR, TSS, Value)) %>%
+  distinct() %>%
+  mutate(SurveyNameQr = paste0(SurveyName, "-Q", Quarter))
 
- 
-YrsAct <- YrsAct %>% filter(StockKeyLabel == "mac.27.nea", SurveyNameIndex == "NS-IBTS, IBTS Q1", `Spatial Indicator` == "D95")
-
-
-
-# Define the character strings
-YearRange <- c("2000, 2001, 2002, 2003, 2004, 2005, 2006")
-YearAct <- c("2000, 2001, 2003, 2006, 2005")
-
-# Convert character strings to numeric vectors
-YearRange_vec <- as.integer(unlist(strsplit(YearRange, ", ")))
-YearAct_vec   <- as.integer(unlist(strsplit(YearAct, ", ")))
-
-# Convert vectors to sets
-YearRange_set <- as.integer(unique(YearRange_vec))
-YearAct_set <- as.integer(unique(YearAct_vec))
-
-# Find the missing years using set operations
-missing_years <- setdiff(YearAct_set, YearRange_set)
-YearRange_set[!YearRange_set %in% YearAct_set]
-
-# Print the missing years
-print(missing_years)
-
+ggplot(data = plot_df) +
+  geom_col(aes(y=AUC, x=SurveyNameQr, fill=SurveyNameQr)) +
+  facet_wrap("Indicator") +
+  coord_cartesian(ylim = c(0,1)) +
+  guides(colour   = guide_legend(order = 1)) +
+  geom_hline(yintercept = 0.5, linetype = 2) +
+  ggtitle(stk) +
+  theme(axis.text.x = element_blank())
